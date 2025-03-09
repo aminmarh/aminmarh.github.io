f318cf0aff6cc8ec7dd0b5d13a5241578ecca743
Amin Marheraoui <95435963+aminmarh@users.noreply.github.com>
2025-03-09 19:29:38 +0100

    Déploiement

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -13,7 +13,7 @@ import VerserTaxe from './pages/VerserTaxe';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/">
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
@@ -30,7 +30,7 @@ function App() {
         </main>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
